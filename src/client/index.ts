302659d9a4e2a9780f970738eb95b63d45f2adf1
rpellette <rpellette@gmail.com>
2020-03-24 18:54:06 -0700

    Update index.ts
    hooking up to tracker html

--- src/client/index.ts
+++ src/client/index.ts
@@ -5,8 +5,77 @@ import { Pattern } from "./pattern";
 
 const KICK_URL = 'https://webaudioapi.com/samples/rhythm/kick.wav';
 const SNARE_URL = 'https://webaudioapi.com/samples/rhythm/snare.wav';
-const HI_HAT_URL = 'https://webaudioapi.com/samples/rhythm/hihat.wav'
+const HI_HAT_URL = 'https://webaudioapi.com/samples/rhythm/hihat.wav';
 
+// new prototype example 1.0 
+const song = new Song(80);
+const pattern = new Pattern();
+pattern.reps = 2;
+const track01 = new Track(song);
+song.addPattern(pattern);
+pattern.addTrack(track01);
+
+const kick = new WebNote(KICK_URL, song);
+const hiHat = new WebNote(HI_HAT_URL, song);
+const snare = new WebNote(SNARE_URL, song);
+
+const playButton = document.getElementById('play');
+const stopButton = document.getElementById('stop');
+const trackColumn1 = document.getElementsByClassName('trackColumn')[0];
+const columnList = trackColumn1.getElementsByClassName('rowNote');
+
+//assigning variables for each track row 
+const trackRows = [];
+for (var i = 0; i < columnList.length; i++) {  
+    trackRows.push(columnList[i]);
+    //adding event listeners to each cell
+    let element: HTMLElement = trackRows[i] as HTMLElement;
+    element.onclick = () => element.style.color = "black";
+    element.onclick = () => track01.addNote(hiHat, i);
+}
+
+playButton.onclick = () => song.play();
+
+/*
+// example 0.2
+const oldPlayButton = document.getElementById('button-old');
+const newPlayButton = document.getElementById('button-new');
+const oldSong = new Song(80);
+const kickNote = new WebNote(KICK_URL, oldSong);
+const hiHatNote = new WebNote(HI_HAT_URL, oldSong);
+const snareNote = new WebNote(SNARE_URL, oldSong);
+const kickTrack = new Track(oldSong);
+const hiHatTrack = new Track(oldSong);
+const snareTrack = new Track(oldSong);
+const pat = new Pattern();
+pat.reps = 2;
+
+hiHatTrack.addNote(hiHatNote, 0);
+hiHatTrack.addNote(hiHatNote, 2);
+hiHatTrack.addNote(hiHatNote, 4);
+hiHatTrack.addNote(hiHatNote, 6);
+hiHatTrack.addNote(hiHatNote, 8);
+hiHatTrack.addNote(hiHatNote, 10);
+hiHatTrack.addNote(hiHatNote, 12);
+hiHatTrack.addNote(hiHatNote, 14);
+
+kickTrack.addNote(kickNote, 0);
+kickTrack.addNote(kickNote, 4);
+kickTrack.addNote(kickNote, 8);
+kickTrack.addNote(kickNote, 12);
+snareTrack.addNote(snareNote, 14);
+
+
+pat.addTrack(hiHatTrack);
+pat.addTrack(kickTrack);
+pat.addTrack(snareTrack);
+
+oldSong.addPattern(pat);
+
+newPlayButton.onclick = () => oldSong.play();
+
+/*
+// example 0.1
 const context = new AudioContext();
 const oldPlayButton = document.getElementById('button-old');
 const newPlayButton = document.getElementById('button-new');
@@ -58,39 +127,4 @@ async function play() {
         snare.start(time+ (7 * eightNoteTime));
     }
 }
-
-
-// New example
-const song = new Song(80);
-const kickNote = new WebNote(KICK_URL, song);
-const hiHatNote = new WebNote(HI_HAT_URL, song);
-const snareNote = new WebNote(SNARE_URL, song);
-const kickTrack = new Track(song);
-const hiHatTrack = new Track(song);
-const snareTrack = new Track(song);
-const pat = new Pattern();
-pat.reps = 2;
-
-hiHatTrack.addNote(hiHatNote, 0);
-hiHatTrack.addNote(hiHatNote, 2);
-hiHatTrack.addNote(hiHatNote, 4);
-hiHatTrack.addNote(hiHatNote, 6);
-hiHatTrack.addNote(hiHatNote, 8);
-hiHatTrack.addNote(hiHatNote, 10);
-hiHatTrack.addNote(hiHatNote, 12);
-hiHatTrack.addNote(hiHatNote, 14);
-
-kickTrack.addNote(kickNote, 0);
-kickTrack.addNote(kickNote, 4);
-kickTrack.addNote(kickNote, 8);
-kickTrack.addNote(kickNote, 12);
-snareTrack.addNote(snareNote, 14);
-
-
-pat.addTrack(hiHatTrack);
-pat.addTrack(kickTrack);
-pat.addTrack(snareTrack);
-
-song.addPattern(pat);
-
-newPlayButton.onclick = () => song.play();
+*/
